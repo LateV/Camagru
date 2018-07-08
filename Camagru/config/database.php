d61e876afbd7dddae48c041fba69014b0df990e4
Vitalii BONDARENKO <vibondar@e2r9p8.unit.ua>
2018-07-08 11:15:51 +0300

    clear

--- Camagru/config/database.php
+++
@@ -1,7 +0,0 @@
-<?php
-$DB_NAME = "camagru";
-$DB_DSN = "mysql:dbname=camagru;localhost";
-$DB_DSN_LIGHT = "mysql:localhost:3307";
-$DB_USER = "root";
-$DB_PASSWORD = "lolkek123";
-?>
--- scripts/put_img.js
+++
@@ -1,7 +0,0 @@
-window.onload = function ()
-{
-	var name = "<? echo $json);?>";
-}
-function get_data(data) {
-	// body...
-}
